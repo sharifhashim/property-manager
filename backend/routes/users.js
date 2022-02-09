ed64ca34e85fc18465d250574efe1bb8771942b2
Hashim Sharif <sharif.hashim@gmail.com>
2022-02-09 16:08:05 -0500

    add api login route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,5 +8,17 @@ module.exports = ({ getUsers }) => {
       .catch((err) => res.json({ error: err.message }));
   });
 
+  router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    login(email, password)
+      .then((user) => {
+        if (!user) {
+          res.status(401);
+          return res.send({ message: "Wrong username or password" });
+        }
+        res.json(user);
+      })
+      .catch((error) => res.json({ error: error.message }));
+  });
   return router;
 };
