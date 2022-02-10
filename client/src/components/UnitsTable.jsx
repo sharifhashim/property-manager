e617af79bbfe9e55435d564e8bd547f6311605ec
Hashim Sharif <sharif.hashim@gmail.com>
2022-02-10 17:56:10 -0500

    create table for units

---
+++ client/src/components/UnitsTable.jsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import useApplicationData from "../hooks/useApplicationData";
+
+export default function UnitsTable() {
+  const { state } = useApplicationData();
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Building Name</TableCell>
+            <TableCell>Unit #</TableCell>
+            <TableCell align="right">Owner</TableCell>
+            <TableCell align="right">City</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {state.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.building_name}
+              </TableCell>
+              <TableCell>{row.unit_number}</TableCell>
+              <TableCell align="right">{row.owner}</TableCell>
+              <TableCell align="right">{row.city}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
