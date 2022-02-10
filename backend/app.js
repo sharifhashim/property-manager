3518d3c36035183ed02266f4d7b246b26304d1e1
Hashim Sharif <sharif.hashim@gmail.com>
2022-02-10 16:31:34 -0500

    install and require cors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const db = require("./db");
-const dbHelpers = require("./helpers/dbHelpers")(db);
+const dbHelpers = require("./db/helpers/dbHelpers")(db);
+const cors = require("cors");
 
 const usersRouter = require("./routes/users");
 const buildingsRouter = require("./routes/buildings");
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use("/api/users", usersRouter(dbHelpers));
 app.use("/api/users/:user_id/buildings", buildingsRouter(dbHelpers));
